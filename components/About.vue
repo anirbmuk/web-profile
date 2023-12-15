dbd310008a8b52168cb699461946cf0b5b70b9c6
anirbmuk <anirbmuk@live.in>
2023-12-15 14:21:13 +0100

    UI adjustments

--- components/About.vue
+++ components/About.vue
@@ -2,7 +2,7 @@
   <div class="my-24 md:my-40">
     <article
       v-if="aboutme"
-      class="wrapper mx-auto whitespace-break-spaces text-left text-md md:text xl:max-w-1/2">
+      class="wrapper mx-auto whitespace-break-spaces text-justify text-md md:text xl:max-w-1/2">
       <p
         v-for="content in aboutme"
         :key="content.documentid"
