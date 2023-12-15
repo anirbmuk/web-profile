13eb4afc2bcc2aeb97f15210df0a611df94a60b7
anirbmuk <anirbmuk@live.in>
2023-12-15 14:21:25 +0100

    Intl number format added

--- components/ui/Rating.vue
+++ components/ui/Rating.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex space-x-1" :title="rating + '/' + _maxRating" ratingcontainer>
+  <div class="flex space-x-1" :title="title" ratingcontainer>
     <LazyUiIcon v-for="rating in _fullRating" :icon="'StarFilled'"></LazyUiIcon>
     <LazyUiIcon v-if="_hasHalf" :icon="'StarHalfFilled'"></LazyUiIcon>
     <template v-if="_remaining?.length">
@@ -16,6 +16,8 @@ const props = defineProps({
   },
 });
 
+const { $i18n } = useNuxtApp();
+
 const _minRating = 0;
 const _maxRating = 10;
 
@@ -30,6 +32,10 @@ const rating = computed(() =>
 const _fullRating = computed(() => transform(Math.floor(rating.value)));
 const _hasHalf = computed(() => Math.ceil(rating.value) !== rating.value);
 const _remaining = computed(() => transform(_maxRating - Math.ceil(rating.value)));
+
+const title = computed(
+  () => `${new Intl.NumberFormat($i18n.locale.value).format(rating.value)}/${_maxRating}`,
+);
 
 defineOptions({
   name: 'RatingComponent',
