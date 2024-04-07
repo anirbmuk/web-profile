78735e51731724b6112d0e02d4619e4685962d75
anirbmuk <anirbmuk@live.in>
2024-04-07 17:29:05 +0200

    Added lodash throttle to show back-to-top

--- composables/useScroll.ts
+++ composables/useScroll.ts
@@ -1,11 +1,17 @@
+import throttle from 'lodash/throttle';
+
 export const useScroll = (key = 'scroll') => {
   const scrollState = useState<number>(key, () => 0);
 
-  const callback = () => (scrollState.value = window.scrollY);
+  const callback = throttle(() => (scrollState.value = window.scrollY), 300);
 
   useEventListener('scroll', callback);
 
-  const scrollToTop = () => window.scrollTo(0, 0);
+  const scrollToTop = () => window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: 'smooth', 
+  });
 
   return {
     scrollState,
