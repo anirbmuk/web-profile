66fea3ad6a8b71d6f05317c13e98aa56a75e066b
anirbmuk <anirbmuk@live.in>
2025-04-28 21:30:13 +0200

    Updated class instance return type

--- modules/firebase/src/core/firebase.ts
+++ modules/firebase/src/core/firebase.ts
@@ -6,24 +6,27 @@ import type { FirebaseConfig } from './../types';
 import { fetchCollection } from './firebase.core';
 
 export class FirebaseController {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  private static instance: any | undefined;
+   
+  private static instance: FirebaseController | undefined;
   private app: FirebaseApp | undefined;
 
   private constructor(cfg: FirebaseConfig) {
     this.app = initializeApp(cfg);
   }
 
-  static getInstance(cfg: FirebaseConfig) {
+  static getInstance(cfg: FirebaseConfig): FirebaseController {
     if (!FirebaseController.instance) {
       FirebaseController.instance = new FirebaseController(cfg);
     }
-    return FirebaseController.instance as FirebaseController;
+    return FirebaseController.instance;
   }
 
   async fetch<T>(path: string, limit?: number | undefined) {
     try {
-      return await fetchCollection<T>(this.app!, {
+      if (!this.app) {
+        throw new Error('FirebaseApp instance is undefined');
+      }
+      return await fetchCollection<T>(this.app, {
         collections: [path],
         whereClause: [{
           column: 'visibility',
