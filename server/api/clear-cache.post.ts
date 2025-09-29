ff04e0bb390583b7043b68bc1498ce227dbdbea7
anirbmuk <anirbmuk@live.in>
2025-09-29 11:42:57 +0200

    Improved end-point error-handling

--- server/api/clear-cache.post.ts
+++ server/api/clear-cache.post.ts
@@ -1,10 +1,21 @@
 /* eslint-disable no-console */
+const PROCESS_NAME = 'Clear nitro cache';
+
 export default defineEventHandler(async () => {
-  const storage = useStorage();
-
-  console.warn('WARNING: clearing nitro cache');
-  console.time('NITRO_CACHE');
-
-  await storage.clear();
-  console.timeEnd('NITRO_CACHE');
+  console.time(PROCESS_NAME);
+  try {
+    const storage = useStorage();
+    await storage.clear();
+    return {
+      success: true,
+      message: 'Nitro cache cleared',
+    };
+  } catch (e) {
+    return {
+      success: false,
+      message: (e as Error).message || 'Nitro cache cleared',
+    };
+  } finally {
+    console.timeEnd(PROCESS_NAME);
+  }
 });
