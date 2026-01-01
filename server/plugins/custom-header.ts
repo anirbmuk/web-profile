583c6480f243eb88b295407c291b3521fa1184c6
anirbmuk <anirbmuk@live.in>
2026-01-01 13:41:48 +0100

    Removed x-powered-by header

--- server/plugins/custom-header.ts
+++ server/plugins/custom-header.ts
@@ -1,7 +1,13 @@
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('render:response', ({ headers }) => {
     if (headers) {
-      headers['X-Author'] = 'anirbmuk (github.com/anirbmuk)';
+      headers['x-author'] = 'anirbmuk (github.com/anirbmuk)';
+      if ('X-Powered-By' in headers) {
+        delete headers['X-Powered-By'];
+      }
+      if ('x-powered-by' in headers) {
+        delete headers['x-powered-by'];
+      }
     }
   });
 });
